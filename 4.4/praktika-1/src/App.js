d8bb2d905e460673d63ca212e92dff39a81727ce
Julius <jzabulenas@protonmail.com>
2022-04-12 22:30:03 +0300

    4.4 praktika-1 progress

--- 4.4/praktika-1/src/App.js
+++ 4.4/praktika-1/src/App.js
@@ -1,9 +1,23 @@
 import ControlledInputsMulti from './components/ControlledInputsMulti'
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <ControlledInputsMulti />
+    <div className="container">
+      <header>
+        {/* <nav
+          style={{
+            borderBottom: "solid 1px",
+            paddingBottom: "1rem",
+          }}
+        >
+          <Link to="/">Home</Link>
+        </nav> */}
+      </header>
+
+      <main>
+        <ControlledInputsMulti />
+      </main>
     </div>
   );
 }
--- 4.4/praktika-1/src/components/ControlledInputsMulti.js
+++ 4.4/praktika-1/src/components/ControlledInputsMulti.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
 
 const ControlledInputs = () => {
   const [student, setStudent] = useState({
@@ -6,6 +7,7 @@ const ControlledInputs = () => {
     birthday: '', city: '', program: '', group: ''
   });
   const [students, setStudents] = useState([]);
+  const [edit, setEdit] = useState("")
 
   const handleChange = (e) => {
     const name = e.target.name;
@@ -35,97 +37,103 @@ const ControlledInputs = () => {
   }
 
   const handleEdit = () => {
-    <p>Yo</p>
+    document.getElementById("wrapper").innerHTML = `<p>Yo</p>`;
   }
 
   return (
-    <>
-      <article className='form'>
-        <form onSubmit={handleSubmit}>
-          <div className='form-control'>
-            <label htmlFor='firstName'>First name: </label>
-            <input
-              type='text'
-              id='firstName'
-              name='firstName'
-              value={student.firstName}
-              onChange={handleChange}
-            />
-          </div>
-          <div className='form-control'>
-            <label htmlFor='lastName'>Last name: </label>
-            <input
-              type='text'
-              id='lastName'
-              name='lastName'
-              value={student.lastName}
-              onChange={handleChange}
-            />
-          </div>
-          <div className='form-control'>
-            <label htmlFor='birthday'>Birthday: </label>
-            <input
-              type='date'
-              id='birthday'
-              name='birthday'
-              value={student.birthday}
-              onChange={handleChange}
-            />
-          </div>
-          <div className='form-control'>
-            <label htmlFor='city'>City: </label>
-            <input
-              type='text'
-              id='city'
-              name='city'
-              value={student.city}
-              onChange={handleChange}
-            />
-          </div>
-          <div className='form-control'>
-            <label htmlFor='program'>Program: </label>
-            <input
-              type='text'
-              id='program'
-              name='program'
-              value={student.program}
-              onChange={handleChange}
-            />
-          </div>
-          <div className='form-control'>
-            <label htmlFor='group'>Group: </label>
-            <input
-              type='text'
-              id='group'
-              name='group'
-              value={student.group}
-              onChange={handleChange}
-            />
-          </div>
-          <button type='submit' className='btn'>
-            add student
-          </button>
-        </form>
-      </article>
-
-      <article>
-        {students.map((student) => {
-          const { id, firstName, lastName, birthday, city, program, group } = student;
-
-          return (
-            <div key={id} className='item'>
-              <b>{firstName} {lastName}</b>
-              <p>{birthday}</p>
-              <p>{city}</p>
-              <p>{program}</p>
-              <p>{group}</p>
-              <button onClick={() => handleEdit}>Edit</button>
-              <button onClick={() => handleRemove(id)}>Remove</button>
+    <div className="row">
+      <div className="col-4">
+        <article className='form'>
+          <form onSubmit={handleSubmit}>
+            <div className='form-control'>
+              <label htmlFor='firstName'>First name: </label>
+              <input
+                type='text'
+                id='firstName'
+                name='firstName'
+                value={student.firstName}
+                onChange={handleChange}
+              />
             </div>
-          );
-        })}
-      </article>
-    </>
+            <div className='form-control'>
+              <label htmlFor='lastName'>Last name: </label>
+              <input
+                type='text'
+                id='lastName'
+                name='lastName'
+                value={student.lastName}
+                onChange={handleChange}
+              />
+            </div>
+            <div className='form-control'>
+              <label htmlFor='birthday'>Birthday: </label>
+              <input
+                type='date'
+                id='birthday'
+                name='birthday'
+                value={student.birthday}
+                onChange={handleChange}
+              />
+            </div>
+            <div className='form-control'>
+              <label htmlFor='city'>City: </label>
+              <input
+                type='text'
+                id='city'
+                name='city'
+                value={student.city}
+                onChange={handleChange}
+              />
+            </div>
+            <div className='form-control'>
+              <label htmlFor='program'>Program: </label>
+              <input
+                type='text'
+                id='program'
+                name='program'
+                value={student.program}
+                onChange={handleChange}
+              />
+            </div>
+            <div className='form-control'>
+              <label htmlFor='group'>Group: </label>
+              <input
+                type='text'
+                id='group'
+                name='group'
+                value={student.group}
+                onChange={handleChange}
+              />
+            </div>
+            <button type='submit' className='btn btn-primary'>
+              add student
+            </button>
+          </form>
+        </article>
+
+        <article>
+          {students.map((student) => {
+            const { id, firstName, lastName, birthday, city, program, group } = student;
+
+            return (
+              <div key={id} className='item'>
+                <b>{firstName} {lastName}</b>
+                <p>{birthday}</p>
+                <p>{city}</p>
+                <p>{program}</p>
+                <p>{group}</p>
+                <button onClick={() => setEdit("yo")}>Edit</button>
+                <button onClick={() => handleRemove(firstName, birthday, city, program, group)}>Remove</button>
+              </div>
+            );
+          })}
+        </article>
+      </div>
+
+      <div className="col-4">
+        {edit}
+      </div>
+    </div>
   );
 };
 
